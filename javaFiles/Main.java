a3245f9e359b59cc60f07c4dc5f1b95fab43ffae
kadrouven <ka.drouven@gmail.com>
2016-02-02 22:05:55 +0000

    Reading in and Reading out test

--- javaFiles/Main.java
+++ javaFiles/Main.java
@@ -1,9 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		FileReader fr = new FileReader("baboon.bmp");
-
+	public static void main(String[] args) throws IOException {
+		Steg s = new Steg();
+		//s.hideString("a", "white.bmp");
+		s.hideString(null,"");
+		
 	}
 
 }
--- javaFiles/stego.java
+++ javaFiles/stego.java
@@ -3,101 +3,161 @@ import java.util.*;
 
 class Steg
 {
+	/**
+	 * A constant to hold the number of bits per byte
+	 */
+	private final int byteLength=8;
+
+	/**
+	 * A constant to hold the number of bits used to store the size of the file extracted
+	 */
+	protected final int sizeBitsLength=32;
+	/**
+	 * A constant to hold the number of bits used to store the extension of the file extracted
+	 */
+	protected final int extBitsLength=64;
 
 
+	/**
+	 * Default constructor to create a steg object, doesn't do anything - so we actually don't need to declare it explicitly. Oh well. 
+	 */
 
-/**
- * A constant to hold the number of bits per byte
- */
-private final int byteLength=8;
+	public Steg()
+	{
 
-/**
- * A constant to hold the number of bits used to store the size of the file extracted
- */
-protected final int sizeBitsLength=32;
-/**
- * A constant to hold the number of bits used to store the extension of the file extracted
- */
-protected final int extBitsLength=64;
+	}
 
- 
- /**
-Default constructor to create a steg object, doesn't do anything - so we actually don't need to declare it explicitly. Oh well. 
-*/
+	/**
+	 * A method for hiding a string in an uncompressed image file such as a .bmp or .png
+	 * You can assume a .bmp will be used
+	 * @param cover_filename - the filename of the cover image as a string 
+	 * @param payload - the string which should be hidden in the cover image.
+	 * @return a string which either contains 'Fail' or the name of the stego image which has been 
+	 * written out as a result of the successful hiding operation. 
+	 * You can assume that the images are all in the same directory as the java files
+	 */
+	//TODO you must write this method
+	public String hideString(String payload, String cover_filename)
+	{
 
-public Steg()
-{
+		// --------- TEST ---------
 
-}
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		try {
+			// Input and Output streams
+			in = new FileInputStream("baboon.bmp");
+			out = new FileOutputStream("output.bmp");
 
-/**
-A method for hiding a string in an uncompressed image file such as a .bmp or .png
-You can assume a .bmp will be used
-@param cover_filename - the filename of the cover image as a string 
-@param payload - the string which should be hidden in the cover image.
-@return a string which either contains 'Fail' or the name of the stego image which has been 
-written out as a result of the successful hiding operation. 
-You can assume that the images are all in the same directory as the java files
-*/
-//TODO you must write this method
-public String hideString(String payload, String cover_filename)
-{
-	
-return null;
-} 
-//TODO you must write this method
-/**
-The extractString method should extract a string which has been hidden in the stegoimage
-@param the name of the stego image 
-@return a string which contains either the message which has been extracted or 'Fail' which indicates the extraction
-was unsuccessful
-*/
-public String extractString(String stego_image)
-{
-return null;
-}
-
-//TODO you must write this method
-/**
-The hideFile method hides any file (so long as there's enough capacity in the image file) in a cover image
-
-@param file_payload - the name of the file to be hidden, you can assume it is in the same directory as the program
-@param cover_image - the name of the cover image file, you can assume it is in the same directory as the program
-@return String - either 'Fail' to indicate an error in the hiding process, or the name of the stego image written out as a
-result of the successful hiding process
-*/
-public String hideFile(String file_payload, String cover_image)
-{
-	
-}
-
-//TODO you must write this method
-/**
-The extractFile method extracts any hidden file (so long as there was enough capacity in the image file) in a cover image
-
-@param stego_image - the name of the hidden file to be extracted, you can assume it is in the same directory as the program
-@return String - either 'Fail' to indicate an error in the extraction process, or the name of the file written out as a
-result of the successful extraction process
-*/
-public String extractFile(String stego_image)
-{
+			// copy header to new file
+			for(int i = 0; i < 54; i++){
+				out.write(in.read());
+			}
 
 
-}
+			// go through payload
+			int x= 1;
+			int c;
+			while ((c = in.read()) != -1) {
 
-//TODO you must write this method
-/**
- * This method swaps the least significant bit with a bit from the filereader
- * @param bitToHide - the bit which is to replace the lsb of the byte of the image
- * @param byt - the current byte
- * @return the altered byte
- */
-public int swapLsb(int bitToHide,int byt)
-{		
-	
-}
+				// Change 0th bit to 0
+				//				System.out.println(c);
+				//				System.out.println((byte) (c & ~(1 << 0)));
+
+				// get red byte (LSB)
+				if(x%3 == 0){
+					// get LSB and change to string bit
+					// this is only a test for now
+					out.write((byte) (c & ~(1 << 0)));
+				}else{
+					out.write(c);
+				}
+				x++;
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 
 
+		}finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 
+		// --------- TEST ---------
 
+		return null;
+	} 
+	//TODO you must write this method
+	/**
+	 * The extractString method should extract a string which has been hidden in the stegoimage
+	 * @param the name of the stego image 
+	 * @return a string which contains either the message which has been extracted or 'Fail' which indicates the extraction
+	 * was unsuccessful
+	 */
+	public String extractString(String stego_image)
+	{
+		return null;
+	}
+
+	//TODO you must write this method
+	/**
+	 * The hideFile method hides any file (so long as there's enough capacity in the image file) in a cover image
+	 * 
+	 * @param file_payload - the name of the file to be hidden, you can assume it is in the same directory as the program
+	 * @param cover_image - the name of the cover image file, you can assume it is in the same directory as the program
+	 * @return String - either 'Fail' to indicate an error in the hiding process, or the name of the stego image written out as a
+	 * result of the successful hiding process
+	 */
+	public String hideFile(String file_payload, String cover_image)
+	{
+		return null;
+	}
+
+	//TODO you must write this method
+	/**
+	 * The extractFile method extracts any hidden file (so long as there was enough capacity in the image file) in a cover image
+	 * 
+	 * @param stego_image - the name of the hidden file to be extracted, you can assume it is in the same directory as the program
+	 * @return String - either 'Fail' to indicate an error in the extraction process, or the name of the file written out as a
+	 * result of the successful extraction process
+	 */
+	public String extractFile(String stego_image)
+	{
+		return null;
+
+	}
+
+	//TODO you must write this method
+	/**
+	 * This method swaps the least significant bit with a bit from the filereader
+	 * @param bitToHide - the bit which is to replace the lsb of the byte of the image
+	 * @param byt - the current byte
+	 * @return the altered byte
+	 */
+	public int swapLsb(int bitToHide,int byt)
+	{		
+		return -1;
+	}
 }
