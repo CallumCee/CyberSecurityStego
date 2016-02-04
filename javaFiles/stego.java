62ce5b76725d53d0f00a92a425b3902f4d4bb104
kadrouven <ka.drouven@gmail.com>
2016-02-04 09:18:42 +0000

    Fixed String Hiding (using swapLsb now)

--- javaFiles/stego.java
+++ javaFiles/stego.java
@@ -9,6 +9,10 @@ class Steg
 	private final int byteLength=8;
 
 	/**
+	 * A constant to hold the number of bits used to store the header of the file extracted
+	 */
+	protected final int headerBitsLength=54;
+	/**
 	 * A constant to hold the number of bits used to store the size of the file extracted
 	 */
 	protected final int sizeBitsLength=32;
@@ -16,6 +20,7 @@ class Steg
 	 * A constant to hold the number of bits used to store the extension of the file extracted
 	 */
 	protected final int extBitsLength=64;
+
 
 
 	/**
@@ -40,83 +45,101 @@ class Steg
 	public String hideString(String payload, String cover_filename)
 	{
 
-		// --------- TEST ---------
 
 		FileInputStream in = null;
 		FileOutputStream out = null;
+
+
+		// ---------- Input Cover File ----------
+
+		// Open Input File
 		try {
-			
-			// Input And Output Streams
 			in = new FileInputStream("baboon.bmp");
-			out = new FileOutputStream("output.bmp");
-
-			
-			// Copy Header To Output File
-			for(int i = 0; i < 54; i++){
-				out.write(in.read());
-			}
-
-			
-			// Convert Payload To Binary String (8 byte chunks)
-			String binaryPayload = "";
-			for(char c : payload.toCharArray()){
-				binaryPayload += Integer.toBinaryString(0x100 + c).substring(1);
-			}
-			
-			
-			// Convert Payload Size to Binary String (32 bits)
-			String binaryPayloadSize = Long.toBinaryString(0x100000000L + binaryPayload.length()).substring(1);
-			
-			// No File Extension, Therefore 64 0's
-			String binaryExtension = String.format("%064d", 0);
-
-			// 
-			String binaryOutput = binaryPayloadSize + binaryExtension + binaryPayload;
-
-			int c;
-			int x = 1;
-			int x2= 0;
-			String tmp;
-			while ((c = in.read()) != -1) {
-				// get red byte (LSB)
-				if(x2 < binaryOutput.length()){
-						tmp = (Integer.toBinaryString(0x100 + c).substring(1,8)) + binaryOutput.charAt(x2);
-						out.write(Integer.parseInt(tmp, 2));
-						x2++;
-				}else{
-					out.write(c);
-				}
-				x++;
-			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			if (out != null) {
-				try {
-					out.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			System.err.println("Unable to open " + cover_filename + ".");
+			return "";
 		}
 
-		// --------- TEST ---------
+		// Read Header
+		List<Integer> header = new ArrayList<Integer>();
+		try {
+			for(int i = 0; i <54; i++){
+				header.add(in.read());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Unable to read header of " + cover_filename + ".");
+			return "";
+		}
+
+		// Read Body
+		List<Integer> body = new ArrayList<Integer>();
+		int byt;
+		try {
+			while((byt = in.read()) != -1){
+				body.add(byt);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Unable to read body of " + cover_filename + ".");
+			return "";
+		}
+
+
+		// ---------- Prepare Secret Output ----------
+
+		// Convert Payload To Binary String (8 byte chunks)
+		String binaryPayload = "";
+		for(char c : payload.toCharArray()){
+			binaryPayload += Integer.toBinaryString(0x100 + c).substring(1);
+		}
+
+		// Convert Payload Size to Binary String (32 bits)
+		String binaryPayloadSize = Long.toBinaryString(0x100000000L + binaryPayload.length()).substring(1);
+
+		// No File Extension, Therefore 64 0's
+		String binaryExtension = String.format("%064d", 0);
+
+		// Secret Output
+		String binarySecretOutput = binaryPayloadSize + binaryExtension + binaryPayload;
+
+
+		// ---------- Output File ----------
+
+		// Create Output File
+		try {
+			out = new FileOutputStream("output.bmp");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("Unable to create output file.");
+			return "";
+		}
+
+		// Copy Header To Output File (Unchanged)
+		try {
+			for(int i : header){
+				out.write(i);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Unable to write header to output file.");
+		}
+
+		// Copy Body To Output File (Modified)
+		try {
+			int x = 0;
+			for(int i : body){
+				if(x < binarySecretOutput.length()){
+					i = swapLsb(binarySecretOutput.charAt(x++), i);
+				}
+				out.write(i);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Unable to write modified body to output file.");
+		}
+
 
 		return null;
 	} 
@@ -129,42 +152,42 @@ class Steg
 	 */
 	public String extractString(String stego_image)
 	{
-		
+
 		FileInputStream in = null;
 		try {
-			
+
 			// Input And Output Streams
-		
+
 			in = new FileInputStream("output.bmp");
 
 			in.skip(54);
-			
+
 			String size = "";
 
 			for(int i = 0; i < 32; i++){
 				size += Integer.toBinaryString(0x100 + in.read()).substring(8);
 			}
-			
+
 			int size1 = Integer.parseInt(size, 2);
-			
+
 			String extension = "";
 
 			for(int i = 0; i < 64; i++){
 				extension += Integer.toBinaryString(0x100 + in.read()).substring(8);
 			}
-			
+
 			String recoveredText = "";
 			String recoveredBinary = "";
 
 			for(int i = 0; i < size1; i++){
 				recoveredBinary += Integer.toBinaryString(0x100 + in.read()).substring(8);
 			}
-			
+
 			for(int i = 0; i <= recoveredBinary.length()-8; i += 8)
 			{
 				recoveredText += (char)Integer.parseInt(recoveredBinary.substring(i, i+8), 2);
 			}
-			
+
 			System.out.println(recoveredText);
 
 		} catch (FileNotFoundException e) {
@@ -186,9 +209,9 @@ class Steg
 				}
 			}
 		}
-		
-		
-		
+
+
+
 		return null;
 	}
 
@@ -228,7 +251,7 @@ class Steg
 	 * @return the altered byte
 	 */
 	public int swapLsb(int bitToHide,int byt)
-	{		
-		return -1;
+	{
+		return (byt >> 1 << 1) + bitToHide;
 	}
 }
